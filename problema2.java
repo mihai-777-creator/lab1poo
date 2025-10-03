25e31f60334967dd513997a54fe4f74ff4b7f20b
mihai-777-creator <marinmihail.r@gmail.com>
2025-10-03 13:39:32 +0300

    Update problema2.java

--- problema2.java
+++ problema2.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class problema2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,16 +7,15 @@ public class problema2 {
         int n = sc.nextInt();
         int c = 0;
 
-
-        int c1 = n / 100;
-        int c2 = (n / 10) % 10;
-        int c3 = n % 10;
-
-        if (c1 % 2 != 0) c++;
-        if (c2 % 2 != 0) c++;
-        if (c3 % 2 != 0) c++;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                c++;
+            }
+            n /= 10;
+        }
 
         System.out.println(c);
-    }
 
+
+    }
 }
